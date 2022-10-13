e163991231e8f890f96abb6298ec2831bfe26ef4
Greg Torrillo <greg.torrillo@gmail.com>
2022-10-12 21:22:31 -0400

    deploy render

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyARR0WAdp92My8O9VMwt5hKhdcZmQZZ9ds",
+  authDomain: "icon-cae7e.firebaseapp.com",
+  projectId: "icon-cae7e",
+  storageBucket: "icon-cae7e.appspot.com",
+  messagingSenderId: "212049304706",
+  appId: "1:212049304706:web:4d291c0f1a8dd7b0c9a89b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/requestMethods.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const BASE_URL = "https://icon-api.onrender.com";
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
+
+export const publicRequest = axios.create({
+  baseURL: BASE_URL,
+});
+
+export const userRequest = axios.create({
+  baseURL: BASE_URL,
+  headers: { token: `Bearer ${TOKEN}` },
+});
